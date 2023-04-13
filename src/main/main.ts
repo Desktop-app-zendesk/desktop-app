eec867d95059799a70c9ce7f61dfba99fe901f2f
ayushi-thavrani <130643674+ayushi-thavrani@users.noreply.github.com>
2023-04-13 17:22:03 +0530

    Revert "json server added"

--- src/main/main.ts
+++ src/main/main.ts
@@ -71,8 +71,8 @@ const createWindow = async () => {
 
   mainWindow = new BrowserWindow({
     show: false,
-    width: 300,
-    height: 450,
+    width: 1024,
+    height: 728,
     icon: getAssetPath('icon.png'),
     webPreferences: {
       preload: app.isPackaged
