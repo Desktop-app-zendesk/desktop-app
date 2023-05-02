5f9c3f79c988457524828912f43a3d1f32219708
Aishwarya Patil <aishwarya_patil3@persistent.com>
2023-05-02 19:17:09 +0530

    ticket detail ui changes

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import Filter from 'renderer/common/Filter';
+import Header from './common/Header';
 import './App.css';
 import Tickets from './components/Tickets';
+import TicketDetails from './components/Tickets/TicketDetails';
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Tickets />} />
-      </Routes>
-    </Router>
+    <>
+      <Header />
+      <Filter />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Tickets />} />
+          <Route path="/ticket-details" element={<TicketDetails />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
