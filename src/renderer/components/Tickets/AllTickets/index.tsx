52658f9465e6bc3cab596b5a2fd6099793891745
shubham nemade <shubham_nemade@persistent.com>
2023-05-04 16:46:40 +0530

    first commit

--- src/renderer/components/Tickets/AllTickets/index.tsx
+++ src/renderer/components/Tickets/AllTickets/index.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import ExpandableRowTable from './ExpandableRowTable';
 
 function AllTickets() {
   const navigate = useNavigate();
@@ -6,25 +7,26 @@ function AllTickets() {
     navigate('/ticket-details');
   };
   return (
-    <section>
-      <div
-        className="relative overflow-x-12 cursor-pointer"
-        onClick={() => {
-          handleClick();
-        }}
-        aria-hidden="true"
-      >
-        <table className="w-full text-sm mt-1 text-left">
-          <tbody>
-            <tr className="border-t border-b">
-              <td className="px-6 py-4 text-sm">Open</td>
-              <td className="px-6 py-4 text-sm">Sample Ticket</td>
-              <td className="text-sm">details</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </section>
+    <ExpandableRowTable/>
+    // <section>
+    //   <div
+    //     className="relative overflow-x-12 cursor-pointer"
+    //     onClick={() => {
+    //       handleClick();
+    //     }}
+    //     aria-hidden="true"
+    //   >
+    //     <table className="w-full text-sm mt-1 text-left">
+    //       <tbody>
+    //         <tr className="border-t border-b">
+    //           <td className="px-6 py-4 text-sm">Open</td>
+    //           <td className="px-6 py-4 text-sm">Sample Ticket</td>
+    //           <td className="text-sm">details</td>
+    //         </tr>
+    //       </tbody>
+    //     </table>
+    //   </div>
+    // </section>
   );
 }
 
