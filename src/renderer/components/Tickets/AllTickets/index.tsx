fcb9a0d0872952be8aaa90b83b02b250599f1d62
Shubham Nemade <shubham_nemade@persistent.com>
2023-05-04 15:39:15 +0530

    changes done in UI

--- src/renderer/components/Tickets/AllTickets/index.tsx
+++ src/renderer/components/Tickets/AllTickets/index.tsx
@@ -1,18 +1,11 @@
+import  ExpandableRowTable from './ExpandableRowTable';
+
 function AllTickets() {
   return (
-    <section>
-      <div className="relative overflow-x-12">
-        <table className="w-full text-sm mt-1 text-left">
-          <tbody>
-            <tr className="border-t border-b">
-              <td className="px-6 py-4 text-sm">Open</td>
-              <td className="px-6 py-4 text-sm">Sample Ticket</td>
-              <td className="text-sm">details</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </section>
+    <div>
+    <ExpandableRowTable/>
+    </div>
+    
   );
 }
 
--- src/renderer/components/Tickets/MyTickets/index.tsx
+++ src/renderer/components/Tickets/MyTickets/index.tsx
@@ -1,18 +1,10 @@
-function MyTicket() {
+import ExpandableRowTable from './ExpandableTable';
+
+function MyTicket({}) {
   return (
-    <section>
-      <div className="relative overflow-x-12">
-        <table className="w-full text-sm mt-1 text-left">
-          <tbody>
-            <tr className="border-t border-b">
-              <td className="px-6 py-4 text-sm">Open</td>
-              <td className="px-6 py-4 text-sm">Sample Ticket 1</td>
-              <td className="text-sm">details</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </section>
+    <div>
+      <ExpandableRowTable />
+    </div>
   );
 }
 
