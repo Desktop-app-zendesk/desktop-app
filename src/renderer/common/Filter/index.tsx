e266a1f1c3af148cebb0b4d098276d04f79c2b42
shreya <shreya_parsai@persistent.com>
2023-05-05 19:45:03 +0530

    sidebar has been updated using props .

--- src/renderer/common/Filter/index.tsx
+++ src/renderer/common/Filter/index.tsx
@@ -1,9 +1,44 @@
 import { Funnel, ArrowCircleLeft } from '@phosphor-icons/react';
 import { useState } from 'react';
-import Filters from './Sidebar';
+import FilterSelector from './Sidebar';
 
 function Sidebar() {
   const [showSidebar, setShowSidebar] = useState(false);
+  const ticketOptions = [
+    { value: 'new', label: 'New' },
+    { value: 'open', label: 'Open' },
+    { value: 'pending', label: 'Pending' },
+    { value: 'solved', label: 'Solved' },
+  ];
+  const requesterOptions = [
+    { value: 'any', label: 'Any' },
+    { value: 'assignee', label: '(assignee)' },
+    { value: 'current user', label: '(current user)' },
+  ];
+  const requestDateOptions = [
+    { value: 'any', label: 'In the last 24 hours' },
+    { value: 'assignee', label: 'In the last 6 months' },
+    { value: 'current user', label: 'In the last 7 days' },
+    { value: 'assignee', label: 'In the last year' },
+    { value: 'current user', label: 'Over a year ago' },
+  ];
+  const typeOptions = [
+    { value: 'any', label: 'Any' },
+    { value: 'assignee', label: '-' },
+    { value: 'current user', label: 'Question' },
+    { value: 'assignee', label: 'Incident' },
+    { value: 'current user', label: 'Problem' },
+    { value: 'current user', label: 'Task' },
+  ];
+
+  const priorityOptions = [
+    { value: 'any', label: 'Any' },
+    { value: 'assignee', label: '-' },
+    { value: 'current user', label: 'Low' },
+    { value: 'assignee', label: 'Normal' },
+    { value: 'current user', label: 'High' },
+    { value: 'current user', label: 'Urgent' },
+  ];
 
   return (
     <div className="filter-sidebar">
@@ -25,11 +60,15 @@ function Sidebar() {
         </button>
       )}
       <div
-        className={`top-0 right-0 bg-white p-20 pl-22 pr-23 px-5 py-14 text-white fixed h-full z-40  ease-in-out duration-300
-      }
-    } ${showSidebar ? 'translate-x-0 ' : 'translate-x-full'}`}
+        className={`sidebar-padding ${
+          showSidebar ? 'translate-x-0 ' : 'translate-x-full'
+        }`}
       >
-        <Filters />
+        <FilterSelector label="Ticket Status" options={ticketOptions} />
+        <FilterSelector label="Requester" options={requesterOptions} />
+        <FilterSelector label="Requester" options={requestDateOptions} />
+        <FilterSelector label="Requester" options={typeOptions} />
+        <FilterSelector label="Requester" options={priorityOptions} />
       </div>
     </div>
   );
