f074abf495516c0f0d54cab53cc96845f1b0e2b5
aishwaryapatil70668 <48828442+aishwaryapatil70668@users.noreply.github.com>
2023-05-11 11:38:45 +0530

    Ticket detail UI (#15)
    * css changes
    
    * ticket detail screen changes
    
    ---------
    
    Co-authored-by: Aishwarya Patil <aishwarya_patil3@persistent.com>

--- src/renderer/components/Tickets/TicketDetails/Incident/index.tsx
+++ src/renderer/components/Tickets/TicketDetails/Incident/index.tsx
@@ -1,11 +1,13 @@
 import TicketControlBar from './TicketControlBar';
 import TicketConversation from './TicketConversation';
+import Footer from './Footer';
 
 function Incident() {
   return (
     <div>
       <TicketControlBar />
       <TicketConversation />
+      <Footer />
     </div>
   );
 }
