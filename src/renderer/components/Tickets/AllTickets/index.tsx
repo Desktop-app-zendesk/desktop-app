6e2edc209ef7c83cd0aa0b39b2d1f22cf5d715d9
AmmarAliHabib <koolammar52@gmail.com>
2023-05-12 17:53:56 +0530

    added-mockapi

--- src/renderer/components/Tickets/AllTickets/index.tsx
+++ src/renderer/components/Tickets/AllTickets/index.tsx
@@ -1,6 +1,20 @@
+/* eslint-disable promise/always-return */
+/* eslint-disable no-console */
+import { useEffect } from 'react';
+import axios from 'axios';
 import ExpandableRowTable from './ExpandableRowTable';
 
 function AllTickets() {
+  useEffect(() => {
+    axios
+      .get('http://localhost:3000/tickets')
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return <ExpandableRowTable row={undefined} />;
 }
 
