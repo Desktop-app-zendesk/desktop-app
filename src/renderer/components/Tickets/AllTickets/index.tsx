11e65c349332620c0c93e10a881fb4c68b18793b
Ajinkya Deshmukh <ajinkya400@gmail.com>
2023-05-12 17:34:03 +0530

    first commit (#13)
    * first commit
    
    * error remove
    
    * comment code remove
    
    ---------
    
    Co-authored-by: shubham nemade <shubham_nemade@persistent.com>

--- src/renderer/components/Tickets/AllTickets/index.tsx
+++ src/renderer/components/Tickets/AllTickets/index.tsx
@@ -1,46 +1,7 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import ExpandableRowTable from './ExpandableRowTable';
 
 function AllTickets() {
-  const navigate = useNavigate();
-  const handleClick = () => {
-    navigate('/ticket-details');
-  };
-  useEffect(() => {
-    axios
-      .get('http://localhost:3000/tickets')
-      // eslint-disable-next-line promise/always-return
-      .then((res) => {
-        // eslint-disable-next-line no-console
-        console.log(res.data);
-      })
-      .catch((err) => {
-        // eslint-disable-next-line no-console
-        console.log(err);
-      });
-  }, []);
-  return (
-    <section>
-      <div
-        className="relative overflow-x-12 cursor-pointer"
-        onClick={() => {
-          handleClick();
-        }}
-        aria-hidden="true"
-      >
-        <table className="w-full text-sm mt-1 text-left">
-          <tbody>
-            <tr className="border-t border-b">
-              <td className="px-6 py-4 text-sm">Open</td>
-              <td className="px-6 py-4 text-sm">Sample Ticket</td>
-              <td className="text-sm">details</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </section>
-  );
+  return <ExpandableRowTable row={undefined} />;
 }
 
 export default AllTickets;
