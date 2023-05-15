8453b4dc7208b9560a1a9ed93d688aa20656ce53
AmmarAliHabib <65000817+AmmarAliHabib@users.noreply.github.com>
2023-05-15 16:27:23 +0530

    added-mockapi (#20)
    * added-mockapi
    
    * function created for mockApi

--- src/renderer/components/Tickets/AllTickets/index.tsx
+++ src/renderer/components/Tickets/AllTickets/index.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from 'react';
+import axios from 'axios';
 import ExpandableRowTable from './ExpandableRowTable';
 
 function AllTickets() {
+  const fetchApi = async () => {
+    const apiData = await axios.get('http://localhost:3000/tickets');
+    console.log(apiData); // eslint-disable-line
+  };
+  useEffect(() => {
+    fetchApi();
+  }, []);
   return <ExpandableRowTable row={undefined} />;
 }
 
